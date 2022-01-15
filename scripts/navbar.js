a33a6b707e0fb6f57c002802fb786dd9dc26c3dd
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-01-15 11:39:33 +0000

    try dif btn

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -18,7 +18,7 @@ leftT.appendChild(text);
 
 var login = document.createElement("div");
 login.classList.add("loginBtn");
-login.innerHTML = '<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark" data-width="120" data-height="35"></div>';
+login.innerHTML = '<div class="g-signin2" data-onsuccess="onSignIn"></div>'; //  data-theme="dark" data-width="120" data-height="35"
 
 leftT.appendChild(login);
 
