d612245b2f5e1af5beecc678dc89cb8579ed52ed
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-01-16 09:19:39 +0000

    Temp remove firebase + add in signout func

--- scripts/init.js
+++ scripts/init.js
@@ -1,28 +1,28 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDpQP8TQRrYYUQyn5x7Y2dPxk-DLpzwKv4",
-    authDomain: "speedtimer-developer.firebaseapp.com",
-    projectId: "speedtimer-developer",
-    storageBucket: "speedtimer-developer.appspot.com",
-    messagingSenderId: "140569673932",
-    appId: "1:140569673932:web:32249d8e691bab95518269"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBSCwtyaAuf42EKYU0eJIZKDhrfiKxFjI4",
+//     authDomain: "speedtimer-dev.firebaseapp.com",
+//     projectId: "speedtimer-dev",
+//     storageBucket: "speedtimer-dev.appspot.com",
+//     messagingSenderId: "206519854258",
+//     appId: "1:206519854258:web:b6e12ec3adc0f7c9f195ab"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-function writeUserData(userId, name, email, imageUrl) {
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture: imageUrl,
-        userString: "new user string"
-    });
-}
+// function writeUserData(userId, name, email, imageUrl) {
+//     firebase.database().ref('users/' + userId).set({
+//         username: name,
+//         email: email,
+//         profile_picture: imageUrl,
+//         userString: "new user string"
+//     });
+// }
 
-function setSpecialString(userId, specialString) {
-    firebase.database().ref('users/' + userId).update({
-        userString: specialString
-    });
-}
+// function setSpecialString(userId, specialString) {
+//     firebase.database().ref('users/' + userId).update({
+//         userString: specialString
+//     });
+// }
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
@@ -39,26 +39,26 @@ function onSignIn(googleUser) {
     afterSignIn(userEntity);
 }
 
-// document.getElementById("signoutLink").addEventListener("click", function signOut() {
-//     var auth2 = gapi.auth2.getAuthInstance();
-//     auth2.signOut().then(function () {
-//         localStorage.removeItem("myUserEntity");
-//     });
-// });
-
-function afterSignIn(userProfile) {
-    var googleProfile = userProfile;
-
-    var check = firebase.database().ref('users').orderByKey().equalTo(googleProfile.id).once("value", function (snapshot) {
-        if (snapshot.exists()) {
-            let userData, userSpecialString;
-
-            firebase.database().ref("users/" + googleProfile.id).on("value", (snap) => {
-                userData = snap.val();
-                userSpecialString = userData.userString;
-            });
-        } else {
-            writeUserData(googleProfile.id, googleProfile.username, googleProfile.email, googleProfile.profile_picture);
-        }
+document.getElementById("signoutLink").addEventListener("click", function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        localStorage.removeItem("myUserEntity");
     });
-}
+});
+
+// function afterSignIn(userProfile) {
+//     var googleProfile = userProfile;
+
+//     var check = firebase.database().ref('users').orderByKey().equalTo(googleProfile.id).once("value", function (snapshot) {
+//         if (snapshot.exists()) {
+//             let userData, userSpecialString;
+
+//             firebase.database().ref("users/" + googleProfile.id).on("value", (snap) => {
+//                 userData = snap.val();
+//                 userSpecialString = userData.userString;
+//             });
+//         } else {
+//             writeUserData(googleProfile.id, googleProfile.username, googleProfile.email, googleProfile.profile_picture);
+//         }
+//     });
+// }
